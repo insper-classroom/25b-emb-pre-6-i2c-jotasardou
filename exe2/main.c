f5efdcaaf2c6eb75bb14e4867fd03fdb43b9a9fc
João Pedro Sardou de Oliveira <joaopso2@al.insper.edu.br>
2025-10-16 11:34:17 -0300

    Primeira tentativa ex2

--- exe2/main.c
+++ exe2/main.c
@@ -21,16 +21,22 @@ void i2c_task(void *p) {
     gpio_pull_up(I2C_SDA_GPIO);
     gpio_pull_up(I2C_SCL_GPIO);
 
-    uint8_t buffer[6];
+    // uint8_t buffer[6];
 
-    // read whoami
-    uint8_t reg_address = 0x75;
-    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, &reg_address, 1, true); // true to keep master control of bus
-    i2c_read_blocking(i2c_default, I2C_CHIP_ADDRESS, buffer, 1, false);
-    printf("WHOAMI: 0x%X \n", buffer[0]);
+    // // read whoami
+    // uint8_t reg_address = 0x75;
+    // i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, &reg_address, 1, true); // true to keep master control of bus
+    // i2c_read_blocking(i2c_default, I2C_CHIP_ADDRESS, buffer, 1, false);
+    // printf("WHOAMI: 0x%X \n", buffer[0]);
 
     // TODO
     // Leia o INT_ENABLE e imprima o valor
+    uint8_t buffer[6];
+    // read INT_ENABLE
+    uint8_t reg_address = MPUREG_INT_ENABLE;
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, &reg_address, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, I2C_CHIP_ADDRESS, buffer, 1, false);
+    
     printf("INT_ENABLE: 0x%X \n", buffer[0]);
 
     while (1) {
